b84a4c5ff2f3fc239119fd0dde45ca2c12c0bb32
msu-wone <msu-wone@DESKTOP-VA932SM>
2024-11-11 12:15:58 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GITHUB_181
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
